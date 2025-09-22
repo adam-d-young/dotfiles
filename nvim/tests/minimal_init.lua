66913bf5f347039b8d3596d2e244b04b97f6089f
Adam Young <adam.young@redhat.com>
2025-09-22 11:28:34 -0500

    pass ci

--- nvim/tests/minimal_init.lua
+++ nvim/tests/minimal_init.lua
@@ -4,7 +4,8 @@ vim.g.maplocalleader = ","
 
 -- Add repo root and plenary to runtimepath
 vim.opt.rtp:append(".")
-vim.opt.rtp:append("tests/site/pack/vendor/start/plenary.nvim")
+-- Vendor plenary for tests from local cache (CI clones here)
+vim.opt.rtp:append(".tests/site/pack/vendor/start/plenary.nvim")
 
 -- Load plenary's busted test harness
 require("plenary.busted")
@@ -12,3 +13,6 @@ require("plenary.busted")
 -- Load only what tests need (avoid full plugin/bootstrap for speed)
 require("user.core.options")
 require("user.core.keymaps")
+
+-- Don't let vendor tests under tests/ run by accident
+vim.g._tests_vendor_disabled = true
